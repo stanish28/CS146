af0d75609686e993fbfc57f7c4f3b2dc5ca7f0a5
stanish28 <stanish994@gmail.com>
2024-03-26 22:35:21 -0700

    Lab4

---
+++ Labs/Lab4/tree.py
@@ -0,0 +1,11 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def invertTree(root):
+    if root is None:
+        return None
+    root.left, root.right = invertTree(root.right), invertTree(root.left)
+    return root
